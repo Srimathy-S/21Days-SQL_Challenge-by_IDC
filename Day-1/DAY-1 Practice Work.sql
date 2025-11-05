386798091c0355a55b61f91f524ae77035cf1238
Srimathy S. <153076847+Srimathy-S@users.noreply.github.com>
2025-11-05 23:47:51 +0530

    Day-1

---
+++ Day-1/DAY-1 Practice Work.sql
@@ -0,0 +1,9 @@
+-- Retrieve all columns from the patients table.
+USE hospital;
+SELECT * FROM patients;
+
+-- Select only the patient_id, name, and age columns from the patients table.
+SELECT patient_id, name, age FROM patients;
+
+-- Display the first 10 records from the services_weekly table. 
+SELECT * FROM services_weekly LIMIT 10;
---
+++ Day-1/Day- 1 Challenge.sql
@@ -0,0 +1,3 @@
+-- List all unique hospital services available in the hospital.
+USE hospital;
+SELECT DISTINCT service FROM services_weekly;
